645136f00308e663695e3dd361d3c64bbb998732
Młyński Stanisław <mlynski.stanislaw@gmail.com>
2020-01-16 16:46:18 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp4_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wiek, PLN;
+            bool kino;
+            Console.WriteLine("How old are you?");
+            wiek = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("How much money you got?");
+            PLN = Convert.ToInt32(Console.ReadLine());
+            kino = (wiek >= 18 && PLN >= 20);
+            Console.WriteLine(kino);
+            Console.ReadKey();
+        }
+    }
+}
